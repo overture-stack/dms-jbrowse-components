99fd09eb8b4e817f536b775f755fcd59e75ee83b
Sam Rich <samrichca@users.noreply.github.com>
2023-05-02 11:13:18 -0400

    make selected files options in linear

--- jbrowse/components/JbrowseLinear.tsx
+++ jbrowse/components/JbrowseLinear.tsx
@@ -35,7 +35,7 @@ export const JbrowseLinear = ({
   selectedFiles = [],
 }: {
   options?: LinearViewModel;
-  selectedFiles: JbrowseFileInput[];
+  selectedFiles?: JbrowseFileInput[];
 }) => {
   const [viewState, setViewState] = useState<LinearViewModel>();
 
